2d2cec167f79f468858e5e69ed40b4345a8642f4
snikolchikov <snikolchikov@navicons.ru>
2020-06-17 17:20:33 +0300

    http integration return String response

--- src/main/java/com/sergeynik/integrationhttp/MessagePrinter.java
+++ src/main/java/com/sergeynik/integrationhttp/MessagePrinter.java
@@ -4,13 +4,7 @@ import org.springframework.messaging.Message;
 
 public class MessagePrinter {
 
-	public String print(Message<?> message){
+	public String print(Message<?> message)	{
 		return "From the inbound gateway:  1 message worked";
 	}
-	
-	public void printConsole(Message<?> message){
-		System.out.println("Message was passed to printConsole method.");
-		System.out.println(message);
-	}
-	
 }
